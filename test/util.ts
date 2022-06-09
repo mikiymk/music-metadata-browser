de51a693c57c609f0da0b014e8368f95ab803c78
mikiymk <mikiymk001@gmail.com>
2022-06-09 20:39:37 +0900

    #7 自動修正を適用する

--- test/util.ts
+++ test/util.ts
@@ -1,21 +1,20 @@
 // Utilities for testing
 
-import { Readable } from 'stream';
-import * as path from 'path';
+import { Readable } from "stream";
+import * as path from "path";
 
 /**
  * A mock readable-stream, using string to read from
  */
 export class SourceStream extends Readable {
-
   constructor(private buf: Buffer) {
     super();
   }
 
-  public _read() {
+  public override _read() {
     this.push(this.buf);
     this.push(null); // push the EOF-signaling `null` chunk
   }
 }
 
-export const samplePath = path.join(__dirname, 'samples');
+export const samplePath = path.join(__dirname, "samples");
