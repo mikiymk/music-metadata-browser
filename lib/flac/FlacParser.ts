384b15ccb31b69b77718c04871cc6173e1629fec
mikiymk <mikiymk001@gmail.com>
2022-10-08 19:34:44 +0900

    Flacパーサーのリファクタリング

--- lib/flac/FlacParser.ts
+++ lib/flac/FlacParser.ts
@@ -9,8 +9,6 @@ import { fourCc } from "../parse-unit/iff/four-cc";
 import { bytes } from "../parse-unit/primitive/bytes";
 import { readUnitFromTokenizer } from "../parse-unit/utility/read-unit";
 
-import { BlockType } from "./BlockType";
-
 import type { INativeMetadataCollector } from "../common/INativeMetadataCollector";
 import type { ITokenParser } from "../ParserFactory";
 import type { ITokenizer } from "../strtok3/types";
@@ -20,8 +18,6 @@ const debug = initDebug("music-metadata:parser:FLAC");
 
 export class FlacParser extends AbstractID3Parser {
   private vorbisParser: VorbisParser;
-
-  private padding = 0;
 
   /**
    * Initialize parser with output (metadata), input (tokenizer) & parsing options (options).
@@ -56,29 +52,29 @@ export class FlacParser extends AbstractID3Parser {
     }
   }
 
-  private parseDataBlock(blockHeader: FlacBlockHeader): Promise<void> {
+  private async parseDataBlock(blockHeader: FlacBlockHeader): Promise<void> {
     debug(`blockHeader type=${blockHeader.type}, length=${blockHeader.length}`);
     switch (blockHeader.type) {
-      case BlockType.STREAMINFO:
+      case 0: // Metadata Block Stream Info
         return this.parseBlockStreamInfo(blockHeader.length);
-      case BlockType.PADDING:
-        this.padding += blockHeader.length;
-        break;
-      case BlockType.APPLICATION:
-        break;
-      case BlockType.SEEKTABLE:
-        break;
-      case BlockType.VORBIS_COMMENT:
+
+      case 4: // // Metadata Block Vorbis Comment
         return this.parseComment(blockHeader.length);
-      case BlockType.CUESHEET:
+
+      case 6: // Metadata Block Picture
+        return this.parsePicture(blockHeader.length);
+
+      case 1: // Metadata Block Padding
+      case 2: // Metadata Block Application
+      case 3: // Metadata Block Seek Table
+      case 5: // Metadata Block Cue Sheet
         break;
-      case BlockType.PICTURE:
-        return this.parsePicture(blockHeader.length).then();
+
       default:
-        this.metadata.addWarning(`Unknown block type: ${blockHeader.type as unknown as string}`);
+        this.metadata.addWarning(`Unknown block type: ${blockHeader.type}`);
     }
     // Ignore data block
-    return this.tokenizer.ignore(blockHeader.length).then();
+    await this.tokenizer.ignore(blockHeader.length);
   }
 
   /**
@@ -116,9 +112,9 @@ export class FlacParser extends AbstractID3Parser {
     }
   }
 
-  private async parsePicture(dataLen: number) {
+  private async parsePicture(dataLen: number): Promise<void> {
     if (this.options.skipCovers) {
-      return this.tokenizer.ignore(dataLen);
+      await this.tokenizer.ignore(dataLen);
     } else {
       const picture = await readUnitFromTokenizer(this.tokenizer, flacBlockPicture(dataLen));
       this.vorbisParser.addTag("METADATA_BLOCK_PICTURE", picture);
