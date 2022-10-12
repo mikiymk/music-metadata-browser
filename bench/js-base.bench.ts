ac390f55e7254a65722b9ea99b3aae3bd8113987
mikiymk <mikiymk001@gmail.com>
2022-10-12 17:34:24 +0900

    ベンチマークテストを使用する

---
+++ bench/js-base.bench.ts
@@ -0,0 +1,95 @@
+/* eslint-disable @typescript-eslint/no-throw-literal */
+import { bench, describe } from "vitest";
+
+describe("number or bigint", () => {
+  bench("number", () => {
+    let a = 1;
+
+    a = a + 10;
+    a = a - 5;
+    a = a * 6;
+    a = a / 4;
+
+    if (a !== 9) throw a;
+  });
+
+  bench("bigint", () => {
+    let a = 1n;
+
+    a = a + 10n;
+    a = a - 5n;
+    a = a * 6n;
+    a = a / 4n;
+
+    if (a !== 9n) throw a;
+  });
+});
+
+describe("construct a DataView each time", () => {
+  bench("one dataview", () => {
+    const data = new Uint8Array([0, 1, 2, 3, 4, 5, 6, 7]);
+    const buffer = data.buffer;
+    const dataview = new DataView(buffer);
+
+    let result = 0;
+    for (let i = 0; i < 8; i++) {
+      result += dataview.getUint8(i);
+    }
+
+    if (result !== 28) throw result;
+  });
+
+  bench("each dataview", () => {
+    const data = new Uint8Array([0, 1, 2, 3, 4, 5, 6, 7]);
+    const buffer = data.buffer;
+
+    let result = 0;
+    for (let i = 0; i < 8; i++) {
+      const dataview = new DataView(buffer);
+      result += dataview.getUint8(i);
+    }
+
+    if (result !== 28) throw result;
+  });
+
+  bench("each dataview with parameter", () => {
+    const data = new Uint8Array([0, 1, 2, 3, 4, 5, 6, 7]);
+
+    let result = 0;
+    for (let i = 0; i < 8; i++) {
+      const dataview = new DataView(data.buffer, data.byteOffset, data.byteLength);
+      result += dataview.getUint8(i);
+    }
+
+    if (result !== 28) throw result;
+  });
+});
+
+const throwsError = () => {
+  if (Math.random() > 0.5) throw new Error("error");
+  return 1;
+};
+
+const returnsError = () => {
+  if (Math.random() > 0.5) return new Error("error");
+  return 1;
+};
+
+describe("throw or return error", () => {
+  bench("throw and catch", () => {
+    let result: unknown = 2;
+    try {
+      result = throwsError();
+    } catch (error) {
+      result = error;
+    }
+
+    if (result !== 1 && !(result instanceof Error)) throw result;
+  });
+
+  bench("return", () => {
+    const result = returnsError();
+
+    if (result !== 1 && !(result instanceof Error)) throw result;
+  });
+});
