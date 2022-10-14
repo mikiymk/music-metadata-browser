55f14fe1be2aab42b89cd1a40e71ff999992b196
mikiymk <mikiymk001@gmail.com>
2022-10-14 15:49:39 +0900

    関数をアロー関数に修正する

--- lib/file-type/util.ts
+++ lib/file-type/util.ts
@@ -1,15 +1,13 @@
 import { decodeUtf8 } from "../compat/text-decoder";
 
-import type { IGetToken } from "../token-types";
-
 /**
  *
  * @param str
  * @returns
  */
-export function stringToBytes(str: string) {
+const stringToBytes = (str: string): number[] => {
   return [...str].map((character) => character.codePointAt(0));
-}
+};
 
 /**
  * Checks whether the TAR checksum is valid.
@@ -17,7 +15,7 @@ export function stringToBytes(str: string) {
  * @param offset - TAR header offset.
  * @returns `true` if the TAR checksum is valid, otherwise `false`.
  */
-export function tarHeaderChecksumMatches(buffer: Uint8Array, offset = 0): boolean {
+export const tarHeaderChecksumMatches = (buffer: Uint8Array, offset = 0): boolean => {
   const readSum = Number.parseInt(decodeUtf8(buffer.subarray(148, 154)).replace(/\0.*$/, "").trim(), 8); // Read sum in header
   if (Number.isNaN(readSum)) {
     return false;
@@ -34,16 +32,6 @@ export function tarHeaderChecksumMatches(buffer: Uint8Array, offset = 0): boolea
   }
 
   return readSum === sum;
-}
-
-/**
- * ID3 UINT32 sync-safe tokenizer token.
- * 28 bits (representing up to 256MB) integer, the msb is 0 to avoid "false syncsignals".
- */
-export const uint32SyncSafeToken: IGetToken<number> = {
-  get: (buffer: Uint8Array, offset: number) =>
-    (buffer[offset + 3] & 0x7f) | (buffer[offset + 2] << 7) | (buffer[offset + 1] << 14) | (buffer[offset] << 21),
-  len: 4,
 };
 
 /**
@@ -70,6 +58,6 @@ export const check = (buffer: Uint8Array, headers: number[], offset = 0, mask?: 
   return true;
 };
 
-export const checkString = (buffer: Uint8Array, header: string, offset?: number) => {
+export const checkString = (buffer: Uint8Array, header: string, offset?: number): boolean => {
   return check(buffer, stringToBytes(header), offset);
 };
